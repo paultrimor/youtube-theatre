d9c28833db4494425ea58997b62954f2934f190c
Ayush Goyal <agoyal2001@gmail.com>
2017-02-18 16:55:08 -0500

    Organize code

--- app.js
+++ app.js
@@ -4,30 +4,26 @@ var index = require('./routes/index');
 var app = express();
 
 var http = require('http').Server(app);
-var io = require('socket.io')(http); 
+var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
 app.use('/', index);
 
-io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on('disconnect', function(){
-		console.log('a user disconnected'); 
+io.on('connection', function (socket) {
+	console.log('A user connected');
+	socket.on('disconnect', function () {
+		console.log('A user disconnected');
 	});
 
-	socket.on('chat message', function(msg){
-  		console.log('message: ' + msg);
-    	io.emit('chat message', msg);
-  });
+	socket.on('chat message', function (msg) {
+		console.log('Message: ' + msg);
+		io.emit('chat message', msg);
+	});
 
 });
-
-
 
 
 http.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
 });
-
-
