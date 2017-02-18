02002ae7201493e021c34d07870bab64835ad5bb
Paul Trimor <trimor.paul@gmail.com>
2017-02-18 13:50:03 -0500

    listen to port 3000

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http); 
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
